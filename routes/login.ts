6e5fcbd28df25eb351ea47e51b8924bc5b4150fd
eduardogsouz <eduardogoncsouza@gmail.com>
2024-07-02 16:18:34 -0300

    feat: add route password recovery generation code

--- routes/login.ts
+++ routes/login.ts
@@ -4,7 +4,33 @@ import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "stdout",
+      level: "error",
+    },
+    {
+      emit: "stdout",
+      level: "info",
+    },
+    {
+      emit: "stdout",
+      level: "warn",
+    },
+  ],
+});
+
+prisma.$on("query", (e) => {
+  console.log("Query: " + e.query);
+  console.log("Params: " + e.params);
+  console.log("Duration: " + e.duration + "ms");
+});
 const router = Router();
 
 router.post("/", async (req, res) => {
@@ -54,6 +80,35 @@ router.post("/", async (req, res) => {
 
       res.status(400).json({ erro: errorDefaultMessage });
     }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post("/recovery", async (req, res) => {
+  const { email } = req.body;
+
+  const errorRecoverMessage = "Email Incorreto ou Inexistente";
+
+  if (!email) {
+    res.status(400).json({ erro: errorRecoverMessage });
+    return;
+  }
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+    if (user == null) {
+      res.status(400).json({ erro: errorRecoverMessage });
+      return;
+    }
+    let recuperationCode = Math.floor(1000 + Math.random() * 9000);
+
+    res.status(200).json({
+      email: user.email,
+      code: recuperationCode,
+    });
   } catch (error) {
     res.status(400).json(error);
   }
